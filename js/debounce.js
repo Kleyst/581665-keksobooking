493a1d44eff69e5fc852cf633deb44dfc5b5f772
Victor Bukhaltsev <victor_bukhaltsev@mail.ru>
2018-03-05 04:12:10 +0700

    Исправил небольшой недочек в debounce

--- js/debounce.js
+++ js/debounce.js
@@ -4,15 +4,13 @@
   var DEBOUNCE_INTERVAL = 500;
   var lastTimeout;
 
-  function debounce(func) {
+  var debounce = function (func) {
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
     }
 
     lastTimeout = window.setTimeout(func, DEBOUNCE_INTERVAL);
-  }
+  };
 
   window.debounce = debounce;
 })();
-
-
